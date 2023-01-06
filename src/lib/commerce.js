f54443af3bf40439c375da08dc4c4c43dc123714
Aman Singh Bhogal <79533183+asbhogal@users.noreply.github.com>
2023-01-06 17:08:11 +0000

    further adjustments

--- src/lib/commerce.js
+++ src/lib/commerce.js
@@ -1,25 +1,25 @@
 import Commerce from "@chec/commerce.js";
 
-const checAPIKey = process.env.REACT_APP_CHEC_PUBLIC_KEY;
-const devEnvironment = process.env.NODE_ENV === 'development';
+export const commerce = new Commerce(process.env.REACT_APP_CHEC_PUBLIC_KEY, true);
 
-const commerceConfig = {
-    axiosConfig: {
-      headers: {
-        'X-Chec-Agent': 'commerce.js/v2',
-        'Chec-Version': '2021-09-29',
-      },
-    },
-  };
+// const checAPIKey = process.env.NEXT_PUBLIC_CHEC_PUBLIC_KEY;
+// const devEnvironment = process.env.NODE_ENV === 'development';
 
-  if (devEnvironment && !checAPIKey) {
-    throw Error('Your public API key must be provided as an environment variable named NEXT_PUBLIC_CHEC_PUBLIC_KEY. Obtain your Chec public key by logging into your Chec account and navigate to Setup > Developer, or can be obtained with the Chec CLI via with the command chec whoami');
-  }
+// const commerceConfig = {
+//     axiosConfig: {
+//       headers: {
+//         'X-Chec-Agent': 'commerce.js/v2',
+//         'Chec-Version': '2021-09-29',
+//       },
+//     },
+//   };
+
+//   if (devEnvironment && !checAPIKey) {
+//     throw Error('Your public API key must be provided as an environment variable named NEXT_PUBLIC_CHEC_PUBLIC_KEY. Obtain your Chec public key by logging into your Chec account and navigate to Setup > Developer, or can be obtained with the Chec CLI via with the command chec whoami');
+//   }
   
-  export default new Commerce(
-    checAPIKey,
-    devEnvironment,
-    commerceConfig,
-  );
-
-//export const commerce = new Commerce(process.env.REACT_APP_CHEC_PUBLIC_KEY, true);
+//   export default new Commerce(
+//     checAPIKey,
+//     devEnvironment,
+//     commerceConfig,
+//   );
