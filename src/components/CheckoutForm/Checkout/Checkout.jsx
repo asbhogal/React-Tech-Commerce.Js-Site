10f572eee5306a9a83364da0ddd5a2fdaf4f059a
Aman Singh Bhogal <79533183+asbhogal@users.noreply.github.com>
2023-01-20 20:30:19 +0000

    added stying for checkout

--- src/components/CheckoutForm/Checkout/Checkout.jsx
+++ src/components/CheckoutForm/Checkout/Checkout.jsx
@@ -22,7 +22,7 @@ const Checkout = () => {
             <main className={ classes.layout }>
                 <Paper className={ classes.paper }>
                     <Typography variant="h4" align="center">Checkout</Typography>
-                    <Stepper activeStep={ activeStep } className={ classes.Stepper }>
+                    <Stepper activeStep={ 0 } className={ classes.Stepper }>
                         {steps.map((step) => (
                             <Step key={ step }>
                                 <StepLabel>{ step }</StepLabel>
