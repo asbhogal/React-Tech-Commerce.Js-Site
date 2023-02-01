416b45ae9b93646f2dea9d0195e393302464b5b6
Aman Singh Bhogal <79533183+asbhogal@users.noreply.github.com>
2023-02-01 15:20:35 +0000

    resolved props.render issue with react-hook-form

--- src/components/CheckoutForm/FormInput.jsx
+++ src/components/CheckoutForm/FormInput.jsx
@@ -1,22 +1,23 @@
 import { TextField, Grid } from "@material-ui/core";
 import { useFormContext, Controller } from "react-hook-form"
 
-const FormInput = () => {
+const FormInput = ({ name, label, required }) => {
 
-    const { control } = useFormContext( { name, label, required });
+    const { control } = useFormContext();
 
     return (
         <Grid item xs={ 12 } sm={ 6 }>
             <Controller 
-                as={ TextField }
+                render={({ field }) => (
+                    <TextField
+                        fullWidth
+                        label={ label }
+                        required
+                    />
+                )}
                 control={ control }
-                fullWidth
-                name={ customerName }
-                label={ label }
-                required={ required }
+                name={ name }
             />
-
-
         </Grid>
     )
 };
